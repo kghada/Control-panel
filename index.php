a7ad307dde78903f9cbfba0ffd32120dc61e3c20
Ghadaxc <80100561+Ghadaxc@users.noreply.github.com>
2021-07-05 02:16:21 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+//connection to server & database 
+$conn = mysqli_connect('localhost', 'root', '', 'base-d' );
+
+//checking the connection 
+if (!$conn) {
+  die("Connection failed :" . mysqli_connect_error());
+} else{
+echo "Connected successfully <br>";}
+
+        if(isset($_POST['Forward'])) { 
+    $sql = ("INSERT INTO Directions(Forward) VALUES ('f')"); 
+    $result = mysqli_query($conn, $sql);
+    if($result){
+         echo "<br><br> Forward value inserted!"; 
+    } else  echo"<br> ERROR: Unable to insert. <br>";
+}
+
+  if(isset($_POST['Backward'])) { 
+    $sql = ("INSERT INTO Directions(Backward) VALUES ('b')"); 
+    $result = mysqli_query($conn, $sql);
+    if($result){
+         echo "<br><br> Backward value inserted!"; 
+    } else  echo"<br> ERROR: Unable to insert. <br>";
+}
+
+  if(isset($_POST['Stop'])) { 
+    $sql = ("INSERT INTO Directions(Stop) VALUES ('s')"); 
+    $result = mysqli_query($conn, $sql);
+    if($result){
+         echo "<br><br> Stop value inserted!"; 
+    } else  echo"<br> ERROR: Unable to insert. <br>";
+}
+
+  if(isset($_POST['Left'])) { 
+    $sql = ("INSERT INTO Directions(L) VALUES ('l')"); 
+    $result = mysqli_query($conn, $sql);
+    if($result){
+         echo "<br><br> Left value inserted!"; 
+    } else  echo"<br> ERROR: Unable to insert. <br>";
+}
+
+  if(isset($_POST['Right'])) { 
+    $sql = ("INSERT INTO Directions(R) VALUES ('r')"); 
+    $result = mysqli_query($conn, $sql);
+    if($result){
+         echo "<br><br> Right value inserted!"; 
+    } else  echo"<br> ERROR: Unable to insert. <br>";
+
+
+
+    
